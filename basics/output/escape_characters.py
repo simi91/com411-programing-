0cebfe3bac15f2006a9e6689af33838b5d4313a6
repl.it user <replituser@example.com>
2021-03-01 21:23:48 +0000

    Escape_characters.py

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+#Displaying escape characters
+
+print("\n Display a new line")
+print("\t Display a tab space")
+print("\\ Display a blackslash")
+print("\ Dislay double quote")
+print("\ Display single quote")
