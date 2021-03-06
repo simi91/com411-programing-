ea0d45f9dd49a9a7426f5c0494b0bbc8a966053e
repl.it user <replituser@example.com>
2021-03-06 23:37:37 +0000

    w2 demo

--- Demos/app.py
+++ Demos/app.py
@@ -1,4 +1,24 @@
 #we wish to create a program that allow us to specify what type of book is being read by beep
 #program should start by asking the user to enter the type of book e.g adventure
 
-print("")
+print("please enter the first whole number")
+n=int(input())
+print ("please enter the second whole number")
+m=int(input())
+print("please enter the third number")
+p=int(input())
+x=0
+y=0
+if(n%2==0)
+  x=x+1
+else:
+  y=y+1
+if(m%2==0)
+  x=x+1
+else:
+  y=y+1
+if(p%2==0)
+  x=x+1
+else:
+  y=y=1
+print("There were {} even numbers and {} add numbers".format(x,y))
--- Demos/w2.py
+++ Demos/w2.py
@@ -1,16 +1,19 @@
-#program that display a menu and allows the user t ether see a nice message, calculate 
+#program that display a menu and allows the user t ether see a nice message, calculate areas of renctagle or triangleor displaya times table for a number.
+
+print("Please choose and option from the menu:\n\n1-Nice message\n2-Area of renctagle\n3-Area of tringle\n4-Times table")
+
 option = int(input())
 
 if option == 1:
   print("Today will be a good day")
-elif option == 2
+elif option == 2:
   print("Enter the lengh of the renctagle:")
   l = int(input())
   print("Enter the width of the renctagle")
   w = int(input())
   area = w*1
   print("The area of this renctangle was {}" .format(area))
-elif option == 3
+elif option == 3:
   print("Enter the base of triangle:")
   base = float(input())
   print("Enter the height of the triangle")
@@ -20,7 +23,9 @@ elif option == 3
 elif option == 4:
   print("What number would you like to see times table for?")
   n = int(input())
-  for i in rangle(1,11,1):
+  for i in range(1,11,1):
     print("{}x{} = {}" .format(n,i,n*i))
-  else:areas of rectangle or triangle or display a times table for a number
+
+else:
+  print("There is not such option.Go to specsavers!")
 
